41fd533d3647fe84d13acbc1dca28203fb45ddd5
oldmlysenko <oldmlysenko@gmail.com>
2025-04-21 23:20:44 +0300

    added textures, tapok in using mouse pos now

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -11,6 +11,7 @@ var direction = Vector2.ZERO
 @onready var current_weapon = load("res://assets/resources/projectiles/shoe.tres") as ProjectileType
 @onready var PROJECTILE = preload("res://entities/projectile/projectile.tscn")
 @onready var shooting_cooldown: Timer = $shooting_cooldown
+@onready var player_sprite: Sprite2D = $player_sprite
 @onready var dummy = %dummy
 
 var shoot_r: bool = true
@@ -23,19 +24,21 @@ func _physics_process(_delta):
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	if velocity != Vector2.ZERO:
-		rotation = lerp_angle(rotation, velocity.angle(), weight)
+		player_sprite.rotation = lerp_angle(player_sprite.rotation, velocity.angle(), weight)
 	
-	if (Input.is_action_pressed("space")) and (shoot_r == true):
+	if (Input.is_action_pressed("lcm")) and (shoot_r == true):
 		shoot_r = false
 		shooting_cooldown.start()
-		shoot()
+		var shoot_ang = get_local_mouse_position().normalized().angle()
+		player_sprite.rotation = shoot_ang
+		shoot(shoot_ang)
 	move_and_slide()
 
-func shoot():
+func shoot(shoot_ang):
 	var projectile = preload("res://entities/projectile/projectile.tscn")
 	var new_projectile = projectile.instantiate()
 	new_projectile.global_position = self.global_position
-	new_projectile.global_rotation = self.global_rotation
+	new_projectile.global_rotation = shoot_ang
 	new_projectile.allience = "player"
 	new_projectile.res = current_weapon
 	dummy.add_child(new_projectile)
