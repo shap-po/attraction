ac2478c0046a3979d6daafd582237624e4058fc2
oldmlysenko <oldmlysenko@gmail.com>
2025-04-21 16:22:13 +0300

    added objects for gp
    + planet object
    + player object and primitive controls

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,28 @@
 extends CharacterBody2D
 
-var speed = 300
+var acceleration = 10
+var friction = 5
+var speed = 0
+var agility = deg_to_rad(0)
+
+var direction = Vector2(1, 0)
 
 func _physics_process(_delta):
-	var direction = Vector2(0, -1)
-	velocity = direction * speed
 	
-	if velocity != Vector2.ZERO:
-		rotation = lerp_angle(rotation, velocity.angle(), 0.5)
-		
+	if Input.is_action_pressed("forwards"):
+		speed += acceleration
+	if Input.is_action_pressed("steerleft"):
+		self.rotation -= agility
+	if Input.is_action_pressed("steerright"):
+		self.rotation += agility
+	
+	if (speed != 0):
+		if (speed >= 0):
+			speed -= friction
+		if (speed <= 0):
+			speed += friction
+		if (abs(speed) <= friction) && (!Input.is_action_pressed("forwards")):
+			speed = 0
+			
+	velocity = speed * Vector2(1, 0).rotated(rotation)	
 	move_and_slide()
