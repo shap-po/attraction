5e53ac2b471295788f3d4a6244b4f9b9548eff44
shap-po <slavko.ukrop@gmail.com>
2025-04-23 23:19:35 +0300

    feat: Implement plant planting & growth

--- entities/plant/plant_type.gd
+++ entities/plant/plant_type.gd
@@ -1,5 +1,16 @@
 extends Resource
 class_name PlantType
 
+const PLANT: PackedScene = preload("res://entities/plant/plant.tscn")
+
 @export var name: String
-@export var sprite: Resource
+@export var texture: Texture2D
+@export var grow_stages: Array[Texture2D]
+
+@export_range(0, 3600)
+var growTime: int ## In seconds
+
+func create() -> Plant:
+	var instance: Plant = PLANT.instantiate()
+	instance.plant_type = self
+	return instance
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
-const speed = 50
-const weight = 0.4
-var allience = "player"
-var direction = Vector2.ZERO
+const speed: int = 50
+const weight: float = 0.4
+var allience: String = "player"
+var direction: Vector2 = Vector2.ZERO
 var health: Health = Health.new(5)
 
-@onready var current_weapon = load("res://assets/resources/projectiles/shoe.tres") as ProjectileType
-@onready var PROJECTILE = preload("res://entities/projectile/projectile.tscn")
+@onready var current_weapon: ProjectileType = preload("res://assets/resources/projectiles/shoe.tres") as ProjectileType
+@onready var current_plant: PlantType = preload("res://assets/resources/plant_types/chem_root.tres") as PlantType
+@onready var PROJECTILE: PackedScene = preload("res://entities/projectile/projectile.tscn")
 @onready var shooting_cooldown: Timer = $shooting_cooldown
 @onready var player_sprite: Sprite2D = $player_sprite
 @onready var dummy = %dummy
+@onready var interaction_area: Area2D = $interaction_area
 
 var shoot_r: bool = true
 
@@ -30,6 +32,8 @@ func _physics_process(_delta):
 		var shoot_ang = get_local_mouse_position().normalized().angle()
 		player_sprite.rotation = shoot_ang
 		shoot(shoot_ang)
+	if Input.is_action_just_pressed("rcm"):
+		interact()
 	move_and_slide()
 
 func shoot(shoot_ang):
@@ -41,6 +45,14 @@ func shoot(shoot_ang):
 	new_projectile.res = current_weapon
 	dummy.add_child(new_projectile)
 
+func interact():
+	var areas: Array[Area2D] = interaction_area.get_overlapping_areas()
+	areas.sort_custom(func(a: Area2D, b: Area2D): return global_position.distance_squared_to(a.global_position) <= global_position.distance_squared_to(b.global_position))
+	var area: Area2D = areas.pop_front()
+	if area == null:
+		return
+	if area is Interactible:
+		var res: Interactible.InteractionResult = area.interact(current_plant)
 
 func _on_shooting_cooldown_timeout() -> void:
 	shoot_r = true
