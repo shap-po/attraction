4f738972e1d71d356510c19d2e6bc6c364bc597f
shap-po <slavko.ukrop@gmail.com>
2025-04-26 17:14:56 +0300

    feat: Add spawn are mask to `WeightedEnemy`

--- entities/enemies/weighted_enemy.gd
+++ entities/enemies/weighted_enemy.gd
@@ -1,5 +1,28 @@
+@tool
 extends Resource
 class_name WeightedEnemy
 
+enum SpawnArea {
+	TOP = 1,
+	BOTTOM = 2,
+	LEFT = 4,
+	RIGHT = 8,
+	FARM = 16
+}
+
 @export var enemy: PackedScene
 @export_range(-100, 100) var weight: int
+var spawn_area_mask: int
+
+func _get_property_list() -> Array[Dictionary]:
+	var proserties: Array[Dictionary] = []
+
+	proserties.append({
+		"name": "spawn_area_mask",
+		"type": TYPE_INT,
+		"usage": PROPERTY_USAGE_DEFAULT,
+		"hint": PROPERTY_HINT_FLAGS,
+		"hint_string": ",".join(SpawnArea.keys().map(func(s: String): return s.to_pascal_case()))
+	})
+
+	return proserties
