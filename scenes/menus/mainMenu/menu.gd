cce3a297fcfd544efa652ce8b4e311582cf09bcd
shap-po <slavko.ukrop@gmail.com>
2025-04-28 22:19:28 +0300

    chore: Finalise merge

--- scenes/menus/mainMenu/menu.gd
+++ scenes/menus/mainMenu/menu.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 func _on_options_button_pressed() -> void:
 	get_tree().current_scene.add_child(options.instantiate())
