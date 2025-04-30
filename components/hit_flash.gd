992c221bb80c4c764c57a0e276b27d76b397efe0
shap-po <slavko.ukrop@gmail.com>
2025-04-30 04:23:44 +0300

    feat: Add `hit_flash` component

---
+++ components/hit_flash.gd
@@ -0,0 +1,34 @@
+## Controls the hit flash effect on the sprite when the player takes damage.
+## This script must be attached to a Health node.
+@tool
+extends Node
+
+const HIT_FLASH: VisualShader = preload("res://assets/shaders/hit_flash.tres")
+@onready var health: Health = $".."
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@export var sprite: Sprite2D:
+	set(val):
+		sprite = val
+		setup()
+
+func _ready() -> void:
+	health.on_damage.connect(on_damage)
+
+func setup() -> void:
+	if sprite == null:
+		return
+	if sprite.material != null:
+		push_warning("Sprite already have a material!")
+		return
+
+	var material: ShaderMaterial = ShaderMaterial.new()
+	material.shader = HIT_FLASH
+	# make sure material changes are local to the node
+	material.resource_local_to_scene = true
+	sprite.material = material
+
+	print("Setup sprite's material")
+
+func on_damage(_dmg: int) -> void:
+	animation_player.play("hit_flash")
