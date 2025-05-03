84cf0869d1795e270e0a186d542bf0a494ce7573
shap-po <50052124+shap-po@users.noreply.github.com>
2025-05-03 21:26:02 +0300

    feat: Implement item swapping

--- scenes/tiles/inventory_grid.gd
+++ scenes/tiles/inventory_grid.gd
@@ -1,24 +1,45 @@
 extends GridContainer
-
 class_name InventoryGrid
 
 @export var inventory: PlayerInventory
-
 @onready var slots: Array = get_children()
+var pressed_index: int = -1
 
 func _ready() -> void:
 	inventory.on_content_changed.connect(update_slot)
 	update_slots()
 
-func get_pressed_index(index):
-	return index
-
-
-func pick_slots() -> void:
-	var slot1
-	var slot2
-	
+func on_open() -> void:
 	pass
+
+func on_close() -> void:
+	if pressed_index != -1:
+		untoggle_slot(pressed_index)
+		pressed_index = -1
+
+func untoggle_slot(index: int) -> void:
+	var slot: ItemButton_3 = slots[index]
+	slot.button.set_pressed_no_signal(false)
+
+func on_pressed(index: int) -> void:
+	if pressed_index == -1:
+		# do not allow selecting an empty slot first
+		if inventory.get_item(index) == null:
+			untoggle_slot(index)
+			return
+
+		pressed_index = index
+		return
+
+	untoggle_slot(pressed_index)
+
+	if pressed_index == index:
+		pressed_index = -1
+		return
+
+	untoggle_slot(index)
+	swap_slots(pressed_index, index)
+	pressed_index = -1
 
 func swap_slots(slot1: int, slot2: int) -> void:
 	inventory.swap_slots(slot1, slot2)
@@ -31,5 +52,3 @@ func update_slot(slot_index: int):
 func update_slots():
 	for i in slots.size():
 		slots[i].update(inventory._content[i])
-		
-		
--- scenes/tiles/item_button_3.gd
+++ scenes/tiles/item_button_3.gd
@@ -1,8 +1,5 @@
 extends AspectRatioContainer
-
-signal button_selected
-
-var slot_index: int
+class_name ItemButton_3
 
 @onready var inventory_grid: InventoryGrid = get_parent()
 @onready var item_visual: TextureRect = $button/itemTexture
@@ -10,42 +7,34 @@ var slot_index: int
 @onready var item_count: Label = $button/itemCount
 @onready var button: Button = $button
 
-func _ready() -> void:
-	button.pressed.connect(selected)
-
-func selected():
-	
-	emit_signal("button_selected", get_index())
-
-func deselected():
-	button.button_up
-
-func hide_all_visuals():
+func hide_all_visuals() -> void:
 	item_visual.visible = false
 	item_price.visible = false
 	item_count.visible = false
-	
-func unhide_all_visuals():
+
+func unhide_all_visuals() -> void:
 	item_visual.visible = true
 	item_price.visible = true
 	item_count.visible = true
 
-func update(item: Item):
+func update(item: Item) -> void:
 	if item == null:
 		hide_all_visuals()
 		return
-	item_visual.visible = true 
+	item_visual.visible = true
 	item_visual.texture = item.item_texture
-	
+
 	if item.buy_price != 0:
 		item_price.visible = true
 		item_price.text = str(item.buy_price)
 	else:
 		item_price.visible = false
-		
+
 	if item is CountableItem:
 		item_count.text = str(item.count)
 		item_count.visible = true
-	else	:
+	else:
 		item_count.visible = false
-		
+
+func _on_button_toggled(_toggled_on: bool) -> void:
+	inventory_grid.on_pressed(get_index())
