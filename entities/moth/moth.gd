d72929923d333dc4550d82e6d6eddc5e2ff0f4f8
shap-po <50052124+shap-po@users.noreply.github.com>
2025-05-04 17:00:11 +0300

    feat: Improve moth nose boop
    Now you have to boop the nose.

--- entities/moth/moth.gd
+++ entities/moth/moth.gd
@@ -1,30 +1,15 @@
 extends TextureRect
 
-@export var default_texture: Texture2D = load("res://assets/textures/moth/Moth girl.png")
-@export var clicked_texture: Texture2D = load("res://assets/textures/moth/Moth girl closed eyes.png")
+const DEFAULT_TEXTURE: Texture2D = preload("res://assets/textures/moth/Moth girl.png")
+const CLICKED_TEXTURE: Texture2D = preload("res://assets/textures/moth/Moth girl closed eyes.png")
 
-@onready var click_sound = $AudioStreamPlayer2D
+@onready var click_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var revert_timer: Timer = $Timer
 
-var revert_timer := Timer.new()
+func _on_button_pressed() -> void:
+	texture = CLICKED_TEXTURE
+	click_sound.play()
+	revert_timer.start()
 
-func _ready():
-	texture = default_texture
-
-	# Set up the timer
-	revert_timer.wait_time = 1.0
-	revert_timer.one_shot = true
-	revert_timer.timeout.connect(_on_revert_texture)
-	add_child(revert_timer)
-	
-	# Optional: if you want to handle mouse click manually
-	set_mouse_filter(Control.MOUSE_FILTER_STOP)
-	connect("gui_input", _on_gui_input)
-
-func _on_gui_input(event: InputEvent):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		texture = clicked_texture
-		click_sound.play()
-		revert_timer.start()
-
-func _on_revert_texture():
-	texture = default_texture
+func _on_timer_timeout() -> void:
+	texture = DEFAULT_TEXTURE
