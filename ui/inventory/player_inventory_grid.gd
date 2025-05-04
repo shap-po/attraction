9b34507edbe6bbadd15c13c3a649dc4257f5ed50
shap-po <50052124+shap-po@users.noreply.github.com>
2025-05-04 21:11:50 +0300

    chore: Center items in the inventory

--- ui/inventory/player_inventory_grid.gd
+++ ui/inventory/player_inventory_grid.gd
@@ -3,6 +3,7 @@ extends BaseInventoryGrid
 class_name PlayerInventoryGrid
 
 var selected_slot: int = -1
+var selected_slot_pos: Vector2 = Vector2.ZERO ## position of the item content in the slot
 
 func _ready() -> void:
 	super._ready()
@@ -12,7 +13,10 @@ func _process(_delta: float) -> void:
 	if selected_slot == -1:
 		return
 
-	slots[selected_slot].item_content.global_position = get_global_mouse_position()
+	var slot: InventorySlot = slots[selected_slot]
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	mouse_pos -= slot.item_content.size / 2
+	slot.item_content.global_position = mouse_pos
 
 func get_inventory() -> PlayerInventory:
 	return inventory as PlayerInventory
@@ -33,6 +37,7 @@ func on_pressed(index: int) -> void:
 		selected_slot = index
 		slots[index].item_content.z_index = 1
 		slots[index].item_content.z_as_relative = true
+		selected_slot_pos = slots[index].item_content.get_position()
 		return
 
 	if selected_slot == index:
@@ -47,7 +52,7 @@ func on_pressed(index: int) -> void:
 
 func reset_slot_content_position(index: int) -> void:
 	var slot: InventorySlot = slots[index]
-	slot.item_content.global_position = slot.button.global_position
+	slot.item_content.set_position(selected_slot_pos)
 	slots[index].item_content.z_index = 0
 	slot.item_content.z_as_relative = false
 
