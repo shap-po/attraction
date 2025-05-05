887be8a3361f756012e4a2e14fddb45f5addd99b
shap-po <50052124+shap-po@users.noreply.github.com>
2025-05-05 06:21:13 +0300

    fix: Mound crashing the game by attacking removed plant

--- entities/enemies/state_machine/behaviours/ants/mound_scout.gd
+++ entities/enemies/state_machine/behaviours/ants/mound_scout.gd
@@ -21,7 +21,7 @@ func on_creation() -> void:
 	time_next_state = 30
 	get_puppet().summon_chances = [0.5, 0.25, 0]
 	get_puppet().brain_ant_killed.connect(on_ant_killed)
-	
+
 	if !map_markers:
 		#print("aw hell nah")
 		return
@@ -43,19 +43,19 @@ func enter() -> void:
 func procces(_delta) -> void:
 	time_next_state -= _delta
 	if time_next_state <= 0:
-		if get_puppet().target is Plant:
+		if not get_puppet().target and get_puppet().target is Plant:
 			var target_loc
 			var dis1 = puppet.target.global_position.distance_squared_to(puppet.global_position)
 			for loc in checkout_locations:
 				var dis2 = loc.global_position.distance_squared_to(puppet.global_position)
-				if dis1 < dis2: 
+				if dis1 < dis2:
 					target_loc = loc
 					dis1 = dis2
 			puppet.target = target_loc
 		else:
 			puppet.target = checkout_locations.pick_random()
 		get_puppet().brain.force_transition("MoundRaid")
-	
+
 func on_ant_killed(type: int):
 	if randf() < 0.5:
 		for child in get_puppet().ants.get_children():
