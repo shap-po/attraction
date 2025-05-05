62b6c0dcd80cdd8920f726b2349cb46e7b957dfe
shap-po <50052124+shap-po@users.noreply.github.com>
2025-05-05 04:29:12 +0300

    feat: Implement insider

--- entities/misc/insider.gd
+++ entities/misc/insider.gd
@@ -1,27 +1,45 @@
 extends Interactible
 
-@onready var main_animation: AnimatedSprite2D = $main_animation
-@onready var listen: Sprite2D = $listen
+@onready var talking_box: TalkingBox = $"../UICanvasLayer/talking_box"
+@onready var spawner: Spawner = %Spawner
+
+@onready var animation: AnimatedSprite2D = $Animation
+@onready var alert: Sprite2D = $Alert
 var anim: bool = false
 
+func enum_to_text(wave_location: WeightedEnemy.SpawnArea) -> String:
+	match wave_location:
+		WeightedEnemy.SpawnArea.TOP:
+			return "North"
+		WeightedEnemy.SpawnArea.BOTTOM:
+			return "South"
+		WeightedEnemy.SpawnArea.RIGHT:
+			return "East"
+		WeightedEnemy.SpawnArea.LEFT:
+			return "West"
+		WeightedEnemy.SpawnArea.FARM:
+			return "Farm"
+	return "Error"
 
 func _ready() -> void:
-	main_animation.play()
-
-func toggle_notification():
-	if listen.visible == true:
-		listen.visible = false
-	else:
-		listen.visible = true
+	animation.play()
 
 func toggle_animation():
 	if anim == true:
-		main_animation.pause()
+		animation.pause()
 		anim = false
 	else:
-		main_animation.play()
+		animation.play()
 		anim = true
 
 func interact(player: Player, item: Item) -> InteractionResult:
-	toggle_notification()
-	return InteractionResult.PASS
+	if not alert.visible:
+		return InteractionResult.PASS
+
+	alert.visible = false
+	talking_box.show_text("Bugs are comming from " + enum_to_text(spawner.wave_location) + " in " + str(int(spawner.next_wave_timer.time_left)) + " seconds")
+	return InteractionResult.SUCCESS
+
+
+func _on_spawner_incoming_wave(_wave_location: WeightedEnemy.SpawnArea, _bug_allert_timeout: int) -> void:
+	alert.visible = true
--- scenes/tiles/talking_box.gd
+++ scenes/tiles/talking_box.gd
@@ -1,5 +1,7 @@
 extends Control
+class_name TalkingBox
 
+@onready var spawner: Spawner = %Spawner
 @onready var sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var label: Label = $PanelContainer/MarginContainer/Label
 @onready var is_talking: bool = false
@@ -10,14 +12,14 @@ func _ready() -> void:
 	visible = false
 	#label.set_visible_characters(0)
 	pass
-	
+
 func talking():
 	pass
 
 func play_sound():
 	pass
 
-func show_text(text_length):
+func _show_text(text_length):
 	is_talking = true
 	sound.play()
 	label.visible_characters = 0
@@ -27,32 +29,12 @@ func show_text(text_length):
 	is_talking = false
 	print("end")
 
-func update():
-	show_text(label.get_total_character_count())
+func show_text(text: String):
+	label.text = text
+	_show_text(label.get_total_character_count())
 	visible = true
 	await get_tree().create_timer(8.0).timeout
 	visible = false
-
-func enum_to_text(wave_location: WeightedEnemy.SpawnArea) -> String:
-	match wave_location:
-		WeightedEnemy.SpawnArea.TOP:
-			return "North"
-		WeightedEnemy.SpawnArea.BOTTOM:
-			return "South"
-		WeightedEnemy.SpawnArea.RIGHT:
-			return "East"
-		WeightedEnemy.SpawnArea.LEFT:
-			return "West"
-		WeightedEnemy.SpawnArea.FARM:
-			return "Farm"
-	return "Error"
-
-func _on_spawner_incoming_wave(wave_location: WeightedEnemy.SpawnArea, bug_allert_timeout: int) -> void:
-	print("incoming wave signal recieved!")
-	var new_text: String
-	new_text = "Bugs are comming from " + enum_to_text(wave_location) + " in " + str(bug_allert_timeout) + " seconds"
-	label.text = new_text
-	update()
 
 
 func _on_audio_stream_player_2d_finished() -> void:
@@ -60,4 +42,3 @@ func _on_audio_stream_player_2d_finished() -> void:
 		var index = randi() % sounds.size()
 		sound.stream = sounds[index]
 		sound.play()
-	
